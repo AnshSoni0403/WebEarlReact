b1aae5ab220993ef66cf76582fc216695107274d
AnshSoniWebEarl <anshsoni.webearl@gmail.com>
2025-06-21 12:24:08 +0530

    preloader everwhere

--- components/common/Preloader.tsx
+++ components/common/Preloader.tsx
@@ -1,19 +1,21 @@
 "use client";
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function Preloader() {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
+  const pathname = usePathname();
 
   useEffect(() => {
     setShow(true);
-    const timer = setTimeout(() => setShow(false), 2000); // 2 seconds
+    const timer = setTimeout(() => setShow(false), 1000); // 1s fade out
     return () => clearTimeout(timer);
-  }, []);
+  }, [pathname]);
 
   if (!show) return null;
 
   return (
-    <div id="preloader" style={{ position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, background: '#fff', zIndex: 999999999, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <div id="preloader" style={{  }}>
       <div id="status">
         <img src="/img/loading.gif" alt="LOADING....!!!!!" />
       </div>
