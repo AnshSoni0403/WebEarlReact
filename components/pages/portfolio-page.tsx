dca849dcb5b612b0711392ce98295b110ae2bbf9
KrishModiWebEarl <krishmodi.webearl@gmail.com>
2025-06-21 15:27:39 +0530

    portfolio hover page done

--- components/pages/portfolio-page.tsx
+++ components/pages/portfolio-page.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import Header from '../header';
 import Footer from '../footer';
 import '../styles/portfolio.css';
+import '../styles/all.css';
 
 const PortfolioPage = () => {
   // Initialize AOS animation on component mount
