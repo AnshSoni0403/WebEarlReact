6bddf569ec84b6f0d3c4d5d404e7a6672f71431c
KrishModiWebEarl <krishmodi.webearl@gmail.com>
2025-06-23 15:59:12 +0530

    SYT single page-1

--- components/PortfolioPages/SYT.tsx
+++ components/PortfolioPages/SYT.tsx
@@ -744,8 +744,8 @@ export default function SYT() {
                 <Image 
                   src="/img/sytlogo.png" 
                   alt="Start Your Tour Logo"
-                  width={99}
-                  height={99}
+                  width={100}
+                  height={100}
                   className="logo-image"
                 />
               </div>
